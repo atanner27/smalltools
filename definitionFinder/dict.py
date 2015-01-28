5f9f87b137789229b48db12a0a052ddcd7e3a232
Andrew Tanner <atanner2727@gmail.com>
2015-01-27 23:07:36 -0500

    clean
    cleaning

--- definitionFinder/dict.py
+++ definitionFinder/dict.py
@@ -4,13 +4,10 @@ glossary = file('gloss.txt')
 glossString = open('gloss.txt').read()
 text_file = open("answers.txt", "w")
 found = False
-#if 'the' in open('gloss.txt').read():
-#print glossString
+
 for line in termfile:
-	#print line.strip()
 	if line.lower().strip() in glossString.lower():
 		index = glossString.lower().find(line.lower().strip())
-		#print glossString.lower().find(".", index, len(glossString))
 		nextPeriod = glossString.lower().find(".", index, len(glossString))
 		text = glossString[index:nextPeriod]
 		text_file.write("Word: " + line.strip() + " \n")
@@ -20,7 +17,4 @@ for line in termfile:
 		print " "
 
 text_file.close()
-	#else:
-		#print "false"
-	#find in otherfile
 
